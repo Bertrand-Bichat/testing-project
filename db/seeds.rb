c184907b80d86957589eefb1dc4afeaef931b55e
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-06-09 12:11:52 +0200

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+require 'faker'
+
+Product.destroy_all
+User.destroy_all
+
+puts 'Creating 5 products...'
+
+5.times do |i|
+  product = Product.create!(
+    name: Faker::Company.name,
+    tagline: Faker::Company.catch_phrase
+  )
+  puts "#{i + 1}. #{product.name}"
+end
+
+puts 'Finished!'
