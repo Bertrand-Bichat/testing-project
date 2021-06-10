3bdae74a20bfc2ac9cdfd4d966423b480a5c028d
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-06-10 15:57:46 +0200

    tests works + add github workflow

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,8 @@ puts 'Creating 5 products...'
   puts "#{i + 1}. #{product.name}"
 end
 
+puts 'Creating 1 user...'
+
+User.create!(email: 'test@gmail.com', password: 'password', first_name: 'Bertrand', last_name: 'Bichat')
+
 puts 'Finished!'
